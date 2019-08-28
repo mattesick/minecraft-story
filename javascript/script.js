05aa84f0ea3aa5d5a42772a0fdd91e81af1a55b5
Mattias <mattias.00@live.se>
2019-08-28 11:07:05 +0200

    fixed buttons and added more minecraft style

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,21 @@
 document.addEventListener("keydown", e => {
     let road;
-    if (e.key == "ArrowRight") {
+    if (e.key == "1") {
         road = document.getElementById("ancorone");
         if (road != null)
             this.document.location.href = road.href;
         //ancorone
-    } else if (e.key == "ArrowUp") {
+    } else if (e.key == "2") {
         road = document.getElementById("ancortwo");
         if (road != null)
             this.document.location.href = road.href;
         //ancortwo
-    } else if (e.key == "ArrowDown") {
+    } else if (e.key == "3") {
         road = document.getElementById("ancorthree");
         if (road != null)
             this.document.location.href = road.href;
         //ancorthree
-    } else if (e.key == "ArrowLeft") {
+    } else if (e.key == "4") {
         road = document.getElementById("ancorfour");
         if (road != null)
             this.document.location.href = road.href;
