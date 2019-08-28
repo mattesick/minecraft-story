59aa820447b251bb4d72bf8936c628ce37e1173e
Mattias <mattias.00@live.se>
2019-08-28 14:58:26 +0200

    added animations

--- javascript/script.js
+++ javascript/script.js
@@ -23,3 +23,38 @@ document.addEventListener("keydown", e => {
     }
 });
 window.history.forward();
+const story = document.getElementsByClassName("story");
+const a1 = document.getElementById("ancorone");
+const a2 = document.getElementById("ancortwo");
+const a3 = document.getElementById("ancorthree");
+const a4 = document.getElementById("ancorfour");
+
+if (document.getElementsByClassName("story") != undefined) {
+    let edit = document.getElementsByClassName("story")[0];
+    let ancorone, ancoortwo, ancorthree, ancorfour;
+    if (a1 != null) { ancorone = a1.innerHTML.split(""); a1.innerHTML = ""; a1.style.display = "none"; }
+    if (a2 != null) { ancortwo = a2.innerHTML.split(""); a2.innerHTML = ""; a2.style.display = "none"; }
+    if (a3 != null) { ancorthree = a3.innerHTML.split(""); a3.innerHTML = ""; a3.style.display = "none"; }
+    if (a4 != null) { ancorfour = a4.innerHTML.split(""); a4.innerHTML = ""; a4.style.display = "none"; }
+    let text = edit.innerHTML;
+    let ancor = 0;
+    edit.innerHTML = "";
+    text = text.split("");
+    let index = 0;
+    let interval = setInterval(e => {
+
+        edit.innerHTML += text[index];
+        index++;
+        if (index == text.length) {
+            index = 0;
+            ancor++;
+            if (a1 != undefined && ancor == 1) { edit = a1; text = ancorone; a1.style.display = "flex"; } else if (ancor == 1) ancor++;
+            if (a2 != undefined && ancor == 2) { edit = a2; text = ancortwo; a2.style.display = "flex"; } else if (ancor == 2) ancor++;
+            if (a3 != undefined && ancor == 3) { edit = a3; text = ancorthree; a3.style.display = "flex"; } else if (ancor == 3) ancor++;
+            console.log(ancor)
+            if (a4 != undefined && ancor == 4) { edit = a4; text = ancorfour; a4.style.display = "flex"; console.log("happend"); } else if (ancor == 4) ancor++;
+            if (ancor > 4) clearInterval(interval);
+            console.log(text);
+        }
+    }, 40);
+}
